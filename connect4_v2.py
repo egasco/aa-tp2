498a15687394f963ffbdc79142aaff48b0ef3db2
Emilio Gasco <gascoe@gmail.com>
2016-11-08 23:43:11 -0300

    * connect4_v2.py -> nueva version, mas parecida al ta te ti

---
+++ connect4_v2.py
@@ -0,0 +1,201 @@
+from __future__ import print_function
+import random
+
+
+def tuple_from_board(board):
+    return tuple([val for row in board for val in row])
+
+
+class Connect4:
+    def __init__(self, playerX, playerO,height,width):
+        self.board = [ [ ' ' for _ in range(width)] for _ in range(height)]
+        self.playerX, self.playerO = playerX, playerO
+        self.width = width
+        self.height = height
+        self.occupancy = [0]*width
+
+    def play_game(self):
+        self.playerX.start_game('X')
+        self.playerO.start_game('O')
+        self.playerX_turn = random.choice([True, False])
+        while True: #yolo
+            if self.playerX_turn:
+                player, color, other_player = self.playerX, 'X', self.playerO
+            else:
+                player, color, other_player = self.playerO, 'O', self.playerX
+            if player.breed == "human":
+                self.display_board()
+            new_pos = player.move(self.board,self.possible_moves())
+            # if self.board[space-1] != ' ': # illegal move
+            #     player.reward(-99, self.board) # score of shame
+            #     break
+            self.move(new_pos)
+            self.board[new_pos[0]][new_pos[1]] = color
+            if self.player_wins(color,new_pos):
+                player.reward(1, self.board,self.possible_moves())
+                other_player.reward(-1, self.board,self.possible_moves())
+                break
+            if self.board_full(): # tie game
+                player.reward(0.5, self.board,self.possible_moves())
+                other_player.reward(0.5, self.board,self.possible_moves())
+                break
+            other_player.reward(0, self.board,self.possible_moves())
+            self.playerX_turn = not self.playerX_turn
+
+    def player_wins(self, color,new_pos):
+        #Arma fila vertical
+        print('new_pos=',new_pos)
+        if new_pos[0] >= 3 and all([ self.board[new_pos[0]-i][new_pos[1]]==color for i in range(0,4) ]):
+            print('Player Wins vertical Line!!!')
+            return True 
+        #Arma linea horizontal
+        init=max(0,new_pos[1]-3)
+        end=min(self.height-4,new_pos[1])
+        for i in range(init,end+1):
+            if all([item==color for item in self.board[new_pos[0]][i:i+4]]):
+                print('Player Wins Horizontal Line!!!')
+                return True
+        #Arma diagonal pendiente positiva
+        dr = new_pos[0] - max(0,new_pos[0]-3)
+        dc = new_pos[1] - max(0,new_pos[1]-3)
+        delta = min(dr,dc)
+        init=(new_pos[0]-delta,new_pos[1]-delta)
+        while init[0]+3 <= self.height-1 and init[1]+3 <= self.width-1 and (init[0] <= new_pos[0] and init[1] <= new_pos[1]): 
+            if all([self.board[init[0]+i][init[1]+i]==color for i in range(0,4) ]):
+                print('Player Wins Positive Diagonal Line!!!')
+                return True
+            print(init[0])
+            print(init[1])
+            init= (init[0] + 1,init[1] + 1)
+        #Arma diagonal pendiente negativa
+        dr = min(self.height-1,new_pos[0]+3)-new_pos[0]
+        dc = new_pos[1] - max(0,new_pos[1]-3)
+        delta = min(dr,dc)
+        init=(new_pos[0]+delta,new_pos[1]-delta)
+        while init[0]-3 >= 0  and init[1]+3 <= self.width-1 and (init[0] >= new_pos[0] and init[1] <= new_pos[1]): 
+            if all([self.board[init[0]-i][init[1]+i]==color for i in range(0,4) ]):
+                print('Player Wins Negative Diagonal Line!!!')
+                return True
+            init= (init[0] - 1,init[1] + 1)
+
+        return False
+
+    def board_full(self):
+        return not any([val==' ' for row in self.board for val in row])
+
+    def display_board(self):
+        #print(self.board)
+        for row in range(0,self.height):
+            print('|',end='')
+            for col in range(0,self.width):
+                print(self.board[self.height-1-row][col],'|',end='')
+            print('')
+    def possible_moves(self):
+        return [(self.occupancy[c],c) for c in range(0,self.width) if self.occupancy[c] < self.height ]
+
+    def move(self,new_pos):
+        self.occupancy[new_pos[1]] += 1
+
+
+class Player(object):
+    def __init__(self):
+        self.breed = "human"
+
+    def start_game(self, color):
+        print("\nNew game!")
+
+    def move(self, board,possible_moves):
+        print('Your  turn!')
+        my_move = -1;
+        while my_move < 0 or my_move > len(possible_moves)-1    :
+            print('Select item from move list: ',possible_moves)
+            print('Enter value from 0 to ',len(possible_moves)-1,' :',end='')
+            my_move = int(raw_input("Your move? "))
+        return possible_moves[my_move]
+
+    def reward(self, value, board,possible_moves):
+        print (self.breed,' rewarded: ',value)
+
+    # def available_moves(self, board):
+    #     return [i+1 for i in range(0,9) if board[i] == ' ']
+
+
+class RandomPlayer(Player):
+    def __init__(self):
+        self.breed = "random"
+
+    def reward(self, value, board,possible_moves):
+        pass
+
+    def start_game(self, color):
+        pass
+
+    def move(self, board,possible_moves):
+        return random.choice(possible_moves)
+
+# class QLearningPlayer(Player):
+#     def __init__(self, epsilon=0.2, alpha=0.3, gamma=0.9):
+#         self.breed = "Qlearner"
+#         self.harm_humans = False
+#         self.q = {} # (state, action) keys: Q values
+#         self.epsilon = epsilon # e-greedy chance of random exploration
+#         self.alpha = alpha # learning rate
+#         self.gamma = gamma # discount factor for future rewards
+
+#     def start_game(self, color):
+#         self.last_board = (' ',)*9
+#         self.last_move = None
+
+#     def getQ(self, state, action):
+#         # encourage exploration; "optimistic" 1.0 initial values
+#         if self.q.get((state, action)) is None:
+#             self.q[(state, action)] = 1.0
+#         return self.q.get((state, action))
+
+#     def move(self, board):
+#         self.last_board = tuple(board)
+#         actions = self.available_moves(board)
+
+#         if random.random() < self.epsilon: # explore!
+#             self.last_move = random.choice(actions)
+#             return self.last_move
+
+#         qs = [self.getQ(self.last_board, a) for a in actions]
+#         maxQ = max(qs)
+
+#         if qs.count(maxQ) > 1:
+#             # more than 1 best option; choose among them randomly
+#             best_options = [i for i in range(len(actions)) if qs[i] == maxQ]
+#             i = random.choice(best_options)
+#         else:
+#             i = qs.index(maxQ)
+
+#         self.last_move = actions[i]
+#         return actions[i]
+
+#     def reward(self, value, board):
+#         if self.last_move:
+#             self.learn(self.last_board, self.last_move, value, tuple(board))
+
+#     def learn(self, state, action, reward, result_state):
+#         prev = self.getQ(state, action)
+#         maxqnew = max([self.getQ(result_state, a) for a in self.available_moves(state)])
+#         self.q[(state, action)] = prev + self.alpha * ((reward + self.gamma*maxqnew) - prev)
+
+
+p1 = RandomPlayer()
+# p1 = MinimaxPlayer()
+# p1 = MinimuddledPlayer()
+# p1 = QLearningPlayer()
+p2 = Player()
+
+for i in xrange(0,200000):
+    t = Connect4(p1, p2,5,4)
+    t.play_game()
+
+# p1 = Player()
+# p2.epsilon = 0
+
+# while True:
+#     t = Connect4(p1, p2)
+# t.play_game()
